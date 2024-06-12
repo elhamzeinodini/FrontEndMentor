32b515ece72abc6faafd7a72d0c18f61d5addd97
elhamzeinodini <elhammth@gmail.com>
2024-06-12 21:51:10 +0330

    update: finish implementing the css for mobile

--- contact-us-form/pages/index.vue
+++ contact-us-form/pages/index.vue
@@ -5,22 +5,22 @@
 
       <div class="inputs">
         <div class="input-wrapper">
-          <label>First Name</label>
+          <label class="input-label">First Name</label>
           <input type="text" v-model="newForm.firstName" />
         </div>
 
         <div class="input-wrapper">
-          <label>Last Name</label>
+          <label class="input-label">Last Name</label>
           <input type="text" v-model="newForm.lastName" />
         </div>
 
         <div class="input-wrapper">
-          <label>Email Address</label>
+          <label class="input-label">Email Address</label>
           <input type="text" v-model="newForm.email" />
         </div>
 
         <div class="input-wrapper">
-          <label>Query Type</label>
+          <label class="input-label">Query Type</label>
 
           <div class="general-req">
             <input type="radio" v-model="newForm.isGeneralReq" />
@@ -34,8 +34,8 @@
         </div>
 
         <div class="input-wrapper">
-          <label>Message</label>
-          <input type="text" v-model="newForm.message" />
+          <label class="input-label">Message</label>
+          <textarea v-model="newForm.message" />
         </div>
 
         <div class="input-wrapper">
